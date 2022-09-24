17bed32989003db6922573a32f63f1706bf2b385
MikaelHuppunen <98363447+MikaelHuppunen@users.noreply.github.com>
2022-09-24 23:01:50 +0300

    You can now move pawns two squares at a time when they first move

--- Chessbot1.0.py
+++ Chessbot1.0.py
@@ -8,31 +8,40 @@ def printboard():
     for i in range(8):
         print(board[i]);
 
+def pawnmove(a, b, c):
+    if b > 0:
+        if c < 8 and (c-a == 1 or (c-a == 2 and a == 1)):
+            return True;
+        return False;
+    elif b < 0:
+        if c >= 0 and (a-c == 1 or (a-c == 2 and a == 6)):
+            return True;
+        return False;
+    return False;
+
 def movepawn():
-    move = int(input());
     global turn;
-    if move > 0 and turn == 0:
+    move = int(input());
+    moveto = int(input());
+    global turn;
+    if (move > 0 and turn == 0) or (move < 0 and turn == 1):
         for i in range(8):
             if move in board[i]:
-                moveto = board[i].index(move);
-                board[i][moveto] = 0;
-                board[i+1][moveto] = move;
-                break
+                if pawnmove(i, move, moveto):
+                    piecefile = board[i].index(move);
+                    board[i][piecefile] = 0;
+                    board[moveto][piecefile] = move;
+                    break
+                else:
+                    print('Illegal move');
+                    return;
         else:
             print('Illegal move');
             return;
-        turn = 1;
-    elif move < 0 and turn == 1:
-        for i in range(8):
-            if move in board[i]:
-                moveto = board[i].index(move);
-                board[i][moveto] = 0;
-                board[i-1][moveto] = move;
-                break
+        if turn == 0:
+            turn = 1;
         else:
-            print('Illegal move');
-            return;
-        turn = 0;
+            turn = 0;
     else:
         print('Illegal move');
         return;
--- readme.txt
+++ readme.txt
@@ -1,1 +1,4 @@
-You can move pawns forward by one square by typing the number assigned to the pawn you wanna move.
+You can move pawns forward by one square by typing the number assigned to the pawn you wanna move and the rank you want to move it to (0 to 7).
+For example:
+1
+3
