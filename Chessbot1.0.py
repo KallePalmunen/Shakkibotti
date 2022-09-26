02674cc7cbcec58bb8f0a7a176d0565d0e07d3c7
MikaelHuppunen <98363447+MikaelHuppunen@users.noreply.github.com>
2022-09-26 16:01:53 +0300

    You can now capture opponent's pawns

--- Chessbot1.0.py
+++ Chessbot1.0.py
@@ -8,29 +8,32 @@ def printboard():
     for i in range(8):
         print(board[i])
 
-def pawnmove(a, b, c, d):
-
-    if b > 0:
-        if c < 8 and (c-a == 1 or (c-a == 2 and a == 1)) and board[c][d] == 0:
-            return True
-        return False
-    elif b < 0:
-        if c >= 0 and (a-c == 1 or (a-c == 2 and a == 6)) and board[c][d] == 0:
-            return True
-        return False
+def pawnmove(n, x0, y0, x1, y1):
+    if x1 < 8 and y1 < 8 and y1 >= 0 and x1 >= 0:
+        if n > 0:
+            if ((y1 == y0 and (x1-x0 == 1 or (x1-x0 == 2 and x0 == 1)) and board[x1][y1] == 0) or 
+                (x1-x0 == 1 and board[x1][y1] < 0 and (y1 - y0 == 1 or y1 - y0 == -1))):
+                return True
+            return False
+        elif n < 0:
+            if ((y1 == y0 and (x0-x1 == 1 or (x0-x1 == 2 and x0 == 6)) and board[x1][y1] == 0) or
+                (x0-x1 == 1 and board[x1][y1] > 0 and (y1 - y0 == 1 or y1 - y0 == -1))):
+                return True
+            return False
     return False
 
 def movepawn():
     global turn
     move = int(input())
-    moveto = int(input())
+    movetox = int(input())
+    movetoy = int(input())
     if (move > 0 and turn == 0) or (move < 0 and turn == 1):
         for i in range(8):
             if move in board[i]:
-                if pawnmove(i, move, moveto, board[i].index(move)):
+                if pawnmove(move, i, board[i].index(move), movetox, movetoy):
                     piecefile = board[i].index(move)
                     board[i][piecefile] = 0
-                    board[moveto][piecefile] = move
+                    board[movetox][movetoy] = move
                     break
                 else:
                     print('Illegal move')
--- readme.txt
+++ readme.txt
@@ -1,4 +1,6 @@
-You can move pawns forward by one square by typing the number assigned to the pawn you wanna move and the rank you want to move it to (0 to 7).
+You can move pawns forward by one square by typing the number assigned to the pawn you wanna move, the rank you want to move it to (0 to 7),
+and the file you want to move it to (0 to 7).
 For example:
 1
 3
+0
