8ed8627c6c78565d1fbc3de0c2e0d616ec8a2966
MikaelHuppunen <98363447+MikaelHuppunen@users.noreply.github.com>
2022-09-26 23:22:39 +0300

    Added knights

--- Chessbot1.0.py
+++ Chessbot1.0.py
@@ -1,6 +1,9 @@
 
-board = [[0,0,0,0,0,0,0,0], [1,2,3,4,5,6,7,8],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0],[-1,-2,-3,-4,-5,-6,-7,-8],[0,0,0,0,0,0,0,0]]
+from itertools import filterfalse
+
+
+board = [[0,10,0,0,0,0,11,0], [1,2,3,4,5,6,7,8],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0],[-1,-2,-3,-4,-5,-6,-7,-8],[0,-10,0,0,0,0,-11,0]]
 
 turn = 0
 
@@ -8,18 +11,40 @@ def printboard():
     for i in range(8):
         print(board[i])
 
-def pawnmove(n, x0, y0, x1, y1):
+def piecemove(n, x0, y0, x1, y1):
     if x1 < 8 and y1 < 8 and y1 >= 0 and x1 >= 0:
-        if n > 0:
-            if ((y1 == y0 and (x1-x0 == 1 or (x1-x0 == 2 and x0 == 1)) and board[x1][y1] == 0) or 
-                (x1-x0 == 1 and board[x1][y1] < 0 and (y1 - y0 == 1 or y1 - y0 == -1))):
+        if abs(n) < 10:
+            if pawnmove(n, x0, y0, x1, y1):
                 return True
             return False
-        elif n < 0:
-            if ((y1 == y0 and (x0-x1 == 1 or (x0-x1 == 2 and x0 == 6)) and board[x1][y1] == 0) or
-                (x0-x1 == 1 and board[x1][y1] > 0 and (y1 - y0 == 1 or y1 - y0 == -1))):
+        if abs(n) < 20 and abs(n) >= 10:
+            if knightmove(n, x0, y0, x1, y1):
                 return True
             return False
+        return False
+    return False
+
+def pawnmove(n, x0, y0, x1, y1):
+    if n > 0:
+        if ((y1 == y0 and (x1-x0 == 1 or (x1-x0 == 2 and x0 == 1 and board[x1-1][y1] == 0)) and board[x1][y1] == 0) or 
+            (x1-x0 == 1 and board[x1][y1] < 0 and (y1 - y0 == 1 or y1 - y0 == -1))):
+            return True
+        return False
+    elif n < 0:
+        if ((y1 == y0 and (x0-x1 == 1 or (x0-x1 == 2 and x0 == 6 and board[x1+1][y1] == 0)) and board[x1][y1] == 0) or
+            (x0-x1 == 1 and board[x1][y1] > 0 and (y1 - y0 == 1 or y1 - y0 == -1))):
+            return True
+        return False
+    return False
+
+def knightmove(n, x0, y0, x1, y1):
+    if ((x1 - x0 == 2 or x1 - x0 == -2) and (y1 - y0 == 1 or y1 - y0 == -1)) or ((x1- x0 == 1 or x1 - x0 == -1) and
+        (y1 - y0 == 2 or y1 - y0 == -2)):
+        if n > 0 and (board[x1][y1] == 0 or board[x1][y1] < 0):
+            return True
+        if n < 0 and (board[x1][y1] == 0 or board[x1][y1] > 0):
+            return True
+        return False
     return False
 
 def movepawn():
@@ -30,7 +55,7 @@ def movepawn():
     if (move > 0 and turn == 0) or (move < 0 and turn == 1):
         for i in range(8):
             if move in board[i]:
-                if pawnmove(move, i, board[i].index(move), movetox, movetoy):
+                if piecemove(move, i, board[i].index(move), movetox, movetoy):
                     piecefile = board[i].index(move)
                     board[i][piecefile] = 0
                     board[movetox][movetoy] = move
--- readme.txt
+++ readme.txt
@@ -1,5 +1,6 @@
-You can move pawns forward by one square by typing the number assigned to the pawn you wanna move, the rank you want to move it to (0 to 7),
-and the file you want to move it to (0 to 7).
+You can move pawns forward by one square by typing the number assigned to the piece or pawn you wanna move, 
+the rank you want to move it to (0 to 7), and the file you want to move it to (0 to 7). 0 to 9 are pawns and 10 to 19 are knights.
+Please note that the vertical dimension is currently labeled x and to vertical dimension is labeled y, for reasons unknown.
 For example:
 1
 3
