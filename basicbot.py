8cb2e2f0c4ad4d7895069388165d4c3fa1ca0b80
Mikael Huppunen <mikael.huppunen@gmail.com>
2022-10-23 04:44:39 +0300

    Basicbot now avoids draws in winning positions

--- basicbot.py
+++ basicbot.py
@@ -81,7 +81,32 @@ def movepieceto2(n, x0, y0, x1, y1):
         Chessbot1.board[x1-int(math.copysign(1, x1 - x0))][y1] = 0
     Chessbot1.moves += 1
 
+def partialrepetition(m):
+    repetitions = 0
+    for i in range(m):
+        if Chessbot1.compareposition(i) and i%2 == m%2:
+            return True
+    return False
+
+def fulleval():
+    evaluation = 0
+    for n in range(1, 51):
+        for x in range(8):
+            if n in Chessbot1.board[x]:
+                evaluation += evaluate2(n, x, Chessbot1.board[x].index(n))
+                break
+    for n in range(1, 51):
+        for x in range(8):
+            if -n in Chessbot1.board[x]:
+                evaluation += evaluate2(-n, x, Chessbot1.board[x].index(-n))
+                break
+    return evaluation
+
+
 def evaluate(n, x0, y0, x1, y1):
+    return evaluate2(n, x1, y1)+evaluate1(n,x0,y0)
+
+def evaluate2(n, x1, y1):
     n1 = Chessbot1.board[min(7, x1 + 1)][min(7, y1+1)]
     n2 = Chessbot1.board[min(7, x1 + 1)][max(0, y1-1)]
     evaluation = math.copysign((n != 0 and abs(n) < 9)*(1+(abs(x1 - 7*(n < 0))*((y1 > 2 and y1 < 6)*((y1 == 5)*0.01+(y1 != 5)*0.05) + 0.001)\
@@ -93,9 +118,12 @@ def evaluate(n, x0, y0, x1, y1):
                 + (abs(n) >= 10 and abs(n) < 20 and y1 > 1 and y1 < 6)*0.01\
                 + ((abs(n) == 50) and (x1 == 0 or x1 == 7) and (y1 == 1 or y1 == 5))*0.05, n)\
                 *(1-((n > 0) and ((n1 < 0 and n1 >-9) or (n2 < 0 and n2 >-9)))*0.5)
+    return evaluation
+
+def evaluate1(n, x0, y0):
     n1 = Chessbot1.board[min(7, x0 + 1)][min(7, y0+1)]
     n2 = Chessbot1.board[min(7, x0 + 1)][max(0, y0-1)]
-    evaluation -= math.copysign((n != 0 and abs(n) < 9)*(1+(abs(x0 - 7*(n < 0))*((y0 > 2 and y0 < 6)*((y0 == 5)*0.01+(y0 != 5)*0.05) + 0.001)\
+    evaluation = -math.copysign((n != 0 and abs(n) < 9)*(1+(abs(x0 - 7*(n < 0))*((y0 > 2 and y0 < 6)*((y0 == 5)*0.01+(y0 != 5)*0.05) + 0.001)\
                 - (Chessbot1.moves < 60 and y0 < 2)*(abs(x0 - 7*(n < 0)) > 2)*0.03))\
                 + 3*(abs(n) > 9 and abs(n) < 30) + \
                 (5+ (not ((x0 == 0 or x0 == 7) and (y0 == 0 or y0 == 7)))*0.01)\
@@ -218,6 +246,7 @@ def bmove(n01, x01, y01, x11, y11):
             if -n in Chessbot1.board[7-i]:
                 x0 = 7-i
                 y0 = Chessbot1.board[x0].index(-n)
+                evaluation2 = evaluate1(-n, x0, y0)
                 if not pinnable(-n, x0, y0, kingx, kingy):
                     for x1 in range(x0 - 2, x0):
                         for y1 in range(y0 - 1, y0+2):
@@ -225,7 +254,7 @@ def bmove(n01, x01, y01, x11, y11):
                                 wastheren = Chessbot1.board[x1][y1]
                                 evaluation1 = evaluate0(x1, y1)
                                 movepieceto2(-n, x0, y0, x1, y1)
-                                movescore3 += evaluate(-n, x0, y0, x1, y1) + evaluation1,
+                                movescore3 += evaluate2(-n, x1, y1) + evaluation1 + evaluation2,
                                 Chessbot1.turn = Chessbot1.bot
                                     
                                 Chessbot1.moves -= 1
@@ -239,7 +268,7 @@ def bmove(n01, x01, y01, x11, y11):
                                 wastheren = Chessbot1.board[x1][y1]
                                 evaluation1 = evaluate0(x1, y1)
                                 movepieceto2(-n, x0, y0, x1, y1)
-                                movescore3 += evaluate(-n, x0, y0, x1, y1) + evaluation1,
+                                movescore3 += evaluate2(-n, x1, y1) + evaluation1 + evaluation2,
                                 Chessbot1.turn = Chessbot1.bot
                                     
                                 Chessbot1.moves -= 1
@@ -253,6 +282,7 @@ def bmove(n01, x01, y01, x11, y11):
             if -n in Chessbot1.board[7-i]:
                 x0 = 7-i
                 y0 = Chessbot1.board[x0].index(-n)
+                evaluation2 = evaluate1(-n, x0, y0)
                 if not pinnable(-n, x0, y0, kingx, kingy):
                     for x1 in range(x0-2, x0+3):
                         for y1 in range(y0-2, y0+3):
@@ -260,7 +290,7 @@ def bmove(n01, x01, y01, x11, y11):
                                 wastheren = Chessbot1.board[x1][y1]
                                 evaluation1 = evaluate0(x1, y1)
                                 movepieceto2(-n, x0, y0, x1, y1)
-                                movescore3 += evaluate(-n, x0, y0, x1, y1) + evaluation1,
+                                movescore3 += evaluate2(-n, x1, y1) + evaluation1 + evaluation2,
                                 Chessbot1.turn = Chessbot1.bot
                                     
                                 Chessbot1.moves -= 1
@@ -274,7 +304,7 @@ def bmove(n01, x01, y01, x11, y11):
                                 wastheren = Chessbot1.board[x1][y1]
                                 evaluation1 = evaluate0(x1, y1)
                                 movepieceto2(-n, x0, y0, x1, y1)
-                                movescore3 += evaluate(-n, x0, y0, x1, y1) + evaluation1,
+                                movescore3 += evaluate2(-n, x1, y1) + evaluation1 + evaluation2,
                                 Chessbot1.turn = Chessbot1.bot
                                     
                                 Chessbot1.moves -= 1
@@ -288,6 +318,7 @@ def bmove(n01, x01, y01, x11, y11):
                 if -n in Chessbot1.board[7-i]:
                     x0 = 7-i
                     y0 = Chessbot1.board[x0].index(-n)
+                    evaluation2 = evaluate1(-n, x0, y0)
                     if not pinnable(-n, x0, y0, kingx, kingy):
                         for x1 in range(8):
                             for y1 in range(8):
@@ -297,7 +328,7 @@ def bmove(n01, x01, y01, x11, y11):
                                     movepieceto2(-n, x0, y0, x1, y1)
                                     if Chessbot1.checkmate(50):
                                         return -500000
-                                    movescore3 += evaluate(-n, x0, y0, x1, y1) + evaluation1,
+                                    movescore3 += evaluate2(-n, x1, y1) + evaluation1 + evaluation2,
                                     Chessbot1.turn = Chessbot1.bot
                                     
                                     Chessbot1.moves -= 1
@@ -313,7 +344,7 @@ def bmove(n01, x01, y01, x11, y11):
                                     movepieceto2(-n, x0, y0, x1, y1)
                                     if Chessbot1.checkmate(50):
                                         return -500000
-                                    movescore3 += evaluate(-n, x0, y0, x1, y1) + evaluation1,
+                                    movescore3 += evaluate2(-n, x1, y1) + evaluation1 + evaluation2,
                                     Chessbot1.turn = Chessbot1.bot
                                     
                                     Chessbot1.moves -= 1
@@ -325,6 +356,7 @@ def bmove(n01, x01, y01, x11, y11):
         if -n in Chessbot1.board[7-i]:
             x0 = 7-i
             y0 = Chessbot1.board[x0].index(-n)
+            evaluation2 = evaluate1(-n, x0, y0)
             for x1 in range(x0-1, x0+2):
                 for y1 in range(y0-1,y0+2):
                     if (Chessbot1.piecemove(-n, x0, y0, x1, y1) 
@@ -334,7 +366,7 @@ def bmove(n01, x01, y01, x11, y11):
                         movepieceto2(-n, x0, y0, x1, y1)
                         if Chessbot1.checkmate(50):
                             return -500000
-                        movescore3 += evaluate(-n, x0, y0, x1, y1) + evaluation1,
+                        movescore3 += evaluate2(-n, x1, y1) + evaluation1 + evaluation2,
                         Chessbot1.turn = Chessbot1.bot
                         
                         Chessbot1.moves -= 1
@@ -436,6 +468,7 @@ def whitemove():
     kingmoved = str(Chessbot1.kingmoved)
     rookmoved = str(Chessbot1.rookmoved)
     pieces = str(Chessbot1.pieces)
+    positions = str(Chessbot1.positions)
     movescore[0].clear()
     for n in range(1, 51):
         for i in range(8):
@@ -447,8 +480,14 @@ def whitemove():
                             and not Chessbot1.pin(n, i, y0, x1, y1)):
                             evaluation1 = evaluate0(x1, y1)
                             movepieceto(n, i, y0, x1, y1)
+                            Chessbot1.positions += deepcopy(Chessbot1.board)
                             Chessbot1.turn = (Chessbot1.bot == 0)
-                            movescore[0] += [blackmove(n, i, y0, x1, y1) + evaluation1]
+                            if(Chessbot1.repetition(Chessbot1.moves) or Chessbot1.stalemate(50) or Chessbot1.stalemate(-50)):
+                                movescore[0] += [(-fulleval()-(blackmove(n, i, y0, x1, y1) + evaluation1))]
+                            elif partialrepetition(Chessbot1.moves):
+                                movescore[0] += [min(0, (-fulleval()-(blackmove(n, i, y0, x1, y1) + evaluation1)))]
+                            else:
+                                movescore[0] += [(blackmove(n, i, y0, x1, y1) + evaluation1)]
                                 
                             Chessbot1.moves = moves
                             Chessbot1.enpassant = enpassant
@@ -456,6 +495,7 @@ def whitemove():
                             Chessbot1.kingmoved = json.loads(kingmoved)
                             Chessbot1.rookmoved = json.loads(rookmoved)
                             Chessbot1.pieces = json.loads(pieces)
+                            Chessbot1.positions = json.loads(positions)
                         else:
                             movescore[0] += [-1000000]
                 break
@@ -478,4 +518,9 @@ def basicbot():
             break
     end = time.time()
     print(end-start)
-    print(max(movescore[0]))
+    if(max(movescore[0]) >= 500000):
+        print("M")
+    elif(max(movescore[0]) <= -500000):
+        print("-M")
+    else:
+        print(fulleval() + max(movescore[0]))
