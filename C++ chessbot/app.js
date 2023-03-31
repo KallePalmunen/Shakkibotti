8eb16835dbf0e1842045a1babc7deb1e9dd4eb92
MikaelHuppunen <98363447+MikaelHuppunen@users.noreply.github.com>
2023-03-31 22:15:20 +0300

    index.html now displays a board

--- C++ chessbot/app.js
+++ C++ chessbot/app.js
@@ -1,9 +1,16 @@
 // Load the WASM module
 Module.onRuntimeInitialized = function () {
+    let ctx = document.getElementById("canvas").getContext("2d");
+    let boardimg = new Image();
+    boardimg.src = "../Images/board.png"
     // Interact with the C++ chess bot using ccall or cwrap
     const SumFunction = Module.cwrap('sum', 'number', ['number', 'number']);
     const movepiece = Module.cwrap('movepiece', 'number', ['string']);
     const basicbot = Module.cwrap('basicbot', '', ['string']);
+
+    function drawBoard(){
+      ctx.drawImage(boardimg, 0, 0);
+    }
     
     // Call the wrapped function
     const result = SumFunction(4,3);
@@ -13,4 +20,5 @@ Module.onRuntimeInitialized = function () {
     console.log(basicbot());
     console.log(movepiece("e7e5"));
     console.log(movepiece("e2e4"));
+    drawBoard();
   };
