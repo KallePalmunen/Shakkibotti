6a2abd08e26abb08359d3ecee72d24f46367837e
MikaelHuppunen <98363447+MikaelHuppunen@users.noreply.github.com>
2023-04-01 21:41:42 +0300

    You are now able to make a move against basicbot

--- C++ chessbot/app.js
+++ C++ chessbot/app.js
@@ -1,24 +1,39 @@
 // Load the WASM module
 Module.onRuntimeInitialized = function () {
-    let ctx = document.getElementById("canvas").getContext("2d");
-    let boardimg = new Image();
-    boardimg.src = "../Images/board.png"
-    // Interact with the C++ chess bot using ccall or cwrap
-    const SumFunction = Module.cwrap('sum', 'number', ['number', 'number']);
-    const movepiece = Module.cwrap('movepiece', 'number', ['string']);
-    const basicbot = Module.cwrap('basicbot', '', ['string']);
+  let ctx = document.getElementById("canvas").getContext("2d");
+  let boardimg = new Image();
+  boardimg.src = "../Images/board.png";
 
-    function drawBoard(){
-      ctx.drawImage(boardimg, 0, 0);
+  // Interact with the C++ chess bot using ccall or cwrap
+  const movepiece = Module.cwrap('movepiece', 'number', ['string']);
+  const basicbot = Module.cwrap('basicbot', 'null', ['string']);
+  const locate_pieces = Module.cwrap('locate_pieces', 'null', []);
+  const add_to_positions = Module.cwrap('add_to_positions', 'null', ['']);
+  const set_can_move_positions = Module.cwrap('set_can_move_positions', 'null', ['']);
+  const printboard = Module.cwrap('printboard', 'null', ['']);
+  const gameend = Module.cwrap('gameend', 'null', ['']);
+
+  function drawBoard(){
+    ctx.drawImage(boardimg, 0, 0);
+  }
+
+  console.log("hi");
+  locate_pieces();
+  add_to_positions();
+  set_can_move_positions();
+  printboard();
+  drawBoard();
+
+  while(true){
+    move = prompt("move");
+    while(!movepiece(move)){
+      console.log("Illegal move");
+      move = prompt("move");
     }
-    
-    // Call the wrapped function
-    const result = SumFunction(4,3);
-    // Update the UI based on the result
-    console.log("hi");
-    console.log(result);
+    set_can_move_positions();
+    gameend();
     console.log(basicbot());
-    console.log(movepiece("e7e5"));
-    console.log(movepiece("e2e4"));
-    drawBoard();
-  };
+    set_can_move_positions();
+    gameend();
+  }
+};
