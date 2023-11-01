d258c5026081841b1f9506e1d3b2c2333d1b2f7a
Kalle J. Palmunen <72195783+KallePalmunen@users.noreply.github.com>
2023-11-01 23:53:27 +0200

    Deleted "kaggles" and added exclamation mark on the "You win'!'" one liner.

--- alphaonetest.py
+++ alphaonetest.py
@@ -365,44 +365,6 @@ class AlphaZero:
             torch.save(self.model.state_dict(), f"model_{iteration}_{self.game}.pt")
             torch.save(self.optimizer.state_dict(), f"optimizer_{iteration}_{self.game}.pt")
 
-import kaggle_environments
-print(kaggle_environments.__version__)
-
-class KaggleAgent:
-    def __init__(self, model, game, args):
-        self.model = model
-        self.game = game
-        self.args = args
-        if self.args['search']:
-            self.mcts = MCTS(self.game, self.args, self.model)
-
-    def run(self, obs, conf):
-        player = obs['mark'] if obs['mark'] == 1 else -1
-        state = np.array(obs['board']).reshape(self.game.row_count, self.game.column_count)
-        state[state==2] = -1
-        
-        state = self.game.change_perspective(state, player)        
-
-        if self.args['search']:
-            policy = self.mcts.search(state)
-
-        else:
-            policy, _ = self.model.predict(state, augment=self.args['augment']) # Not working with the video's implementation
-
-        valid_moves = self.game.get_valid_moves(state)
-        policy *= valid_moves
-        policy /= np.sum(policy)
-
-        if self.args['temperature'] == 0:
-            action = int(np.argmax(policy))
-        elif self.args['temperature'] == float('inf'):
-            action = np.random.choice([r for r in range(self.game.action_size) if policy[r] > 0])
-        else:
-            policy = policy ** (1 / self.args['temperature'])
-            policy /= np.sum(policy)
-            action = np.random.choice(self.game.action_size, p=policy)
-
-        return action
     
 game = TicTacToe()
 
@@ -441,7 +403,7 @@ def play(args):
         print(state)
         if tictactoe.get_value_and_terminated(state, action)[1]:
             if tictactoe.get_value_and_terminated(state, action)[0] == 1:
-                print("You win")
+                print("You win!")
             else:
                 print("tie")
             break
