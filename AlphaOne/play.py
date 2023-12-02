f6095350639f327b5a373c44fbf4300498f0a26f
MikaelHuppunen <98363447+MikaelHuppunen@users.noreply.github.com>
2023-12-03 00:23:24 +0200

    play.py now runs with Chess

--- AlphaOne/play.py
+++ AlphaOne/play.py
@@ -2,14 +2,14 @@ from alphaonetest import *
 
 args = {
     'C': 2,
-    'num_searches': 600,
+    'num_searches': 100,
     'dirichlet_epsilon': 0.1,
     'dirichlet_alpha': 0.3,
     'search': True,
     'temperature': 0,
 }
 
-game = TicTacToe()
+game = Chess()
 model_dict = "model_2_Chess.pt"
 
 play(args, game, model_dict)
