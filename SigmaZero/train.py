58c06a22d5167b18460ba5a5056bd7ce7eb34427
Mikael Huppunen <mikael.huppunen@gmail.com>
2024-02-07 10:06:09 +0200

    Trained with 'num_searches': 100, 'num_iterations': 3, 'num_selfPlay_iterations': 15,

--- SigmaZero/train.py
+++ SigmaZero/train.py
@@ -2,9 +2,9 @@ from sigmazero import *
 
 args = {
     'C': 2,
-    'num_searches': 30,
+    'num_searches': 100,
     'num_iterations': 3,
-    'num_selfPlay_iterations': 5,
+    'num_selfPlay_iterations': 15,
     'num_parallel_games': 100,
     'num_epochs': 4,
     'batch_size': 128,
