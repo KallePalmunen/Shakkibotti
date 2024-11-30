4ed801278e8979fca7e529589e15bef5e2778a13
Kalle J. Palmunen <72195783+KallePalmunen@users.noreply.github.com>
2024-11-30 11:59:06 +0200

    Imports to nemesis.py (lets use rules.py this point forward)

---
+++ python/Nemesis/nemesis.py
@@ -0,0 +1,10 @@
+from SigmaZero.rules import *
+import numpy as np
+print(np.__version__)
+import torch
+print(torch.__version__)
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+import math
+import time
