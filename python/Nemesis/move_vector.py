97cfe66e0b1dcd04dc11a2600b6f8e1a75a8ddbc
leeviloikkanen <lepa.loikkanen@gmail.com>
2025-01-25 21:26:23 +0200

    Added enpassant to material gain

--- python/Nemesis/move_vector.py
+++ python/Nemesis/move_vector.py
@@ -1,5 +1,14 @@
-from SigmaZero.rules import *
 
+import sys
+import os
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Add the parent folder (one level up) to sys.path
+parent_dir = os.path.dirname(script_dir)
+sys.path.append(parent_dir)
+
+from SigmaZero.rules import *
 pawn_position_eval = [[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],[0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5]
 ,[0.1,0.1,0.2,0.3,0.3,0.2,0.1,0.1],[0.05,0.05,0.1,0.25,0.25,0.1,0.05,0.05],[0.0,0.0,0.0,0.2,0.2,0.0,0.0,0.0]
 ,[0.05,-0.05,-0.1,0.0,0.0,-0.1,-0.05,0.05],[0.05,0.1,0.1,-0.2,-0.2,0.1,0.1,0.05],[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]]
@@ -35,8 +44,17 @@ def piece_value(piece):
     return 0
 
 def material_gain(state, piece, y0, x0, y1, x1, color):
-    taken_piece = state[y1,x1]
+    if abs(piece)<10 and (y1==2 and color == -1 ) and state[y1,x1] == 0:
+        if 0<state[y1+1, x1]<10:
+            taken_piece = state[y1+1,x1]
+        
+    elif abs(piece)<10 and (y1 == 5 and color == 1) and state[y1,x1] == 0:
+        if state[y1-1, x1]<0:
+            taken_piece = state[y1-1,x1]
+    else: 
+        taken_piece = state[y1,x1]
     ### TODO: add enpassant material gain
+    
     gain = -color*piece_value(taken_piece)
     if promote(piece, y1):
         gain += 8.0
@@ -71,10 +89,12 @@ def create_move_vector(state, move, color):
     return move_vector
 
 #testing
-state = np.array([[30,10,20,50,40,21,11,31], [1,2,3,4,5,6,7,8],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0],[-1,-2,-3,-4,-5,-6,-7,-8],[-30,-10,-20,-50,-40,-21,-11,-31]])
+state = np.array([[30,10,20,50,40,21,11,31], [1,2,3,4,5,6,0,8],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,7,-8],
+         [0,0,0,0,0,0,0,0],[-1,-2,-3,-4,-5,-6,-7,0],[-30,-10,-20,-50,-40,-21,-11,-31]])
 
-move = [40,0,4,7,4]
+print(state[4,6])
+print(state[5,7])
+move = [7,4,6,5,7]
 color = 1
 move_vector = create_move_vector(state, move, color)
 print(move_vector)
